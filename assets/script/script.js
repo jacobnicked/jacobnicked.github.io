5fb77d05d91e0827d1f9a53691b9a2fe4fd41f09
Jacob Nicked <95031835+jacobnicked@users.noreply.github.com>
2025-01-18 17:39:09 +0100

    Update script.js

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,4 +1,12 @@
+//Menu for mobile
 function toggleMobileMenu() {
     const menu = document.getElementById("mobileMenu");
     menu.style.display = menu.style.display === "block" ? "none" : "block";
 }
+
+//Current year
+function currentYear() {
+    document.getElementById("currentyear").textContent = new Date().getFullYear();
+}
+
+    document.addEventListener('DOMContentLoaded', currentYear);
